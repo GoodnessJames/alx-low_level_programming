96881b91ab1f5aa89e4dd9b797e9902836c78621
GoodnessJames <akomagoodness97@gmail.com>
2023-04-20 10:34:42 -0700

    ALX low level programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <string.h>
+#include <unistd.h>
 
 /**
 * main - Print code line with strlen
@@ -7,7 +7,6 @@
 */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19",
-	strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n);
 	return (1);
 }
