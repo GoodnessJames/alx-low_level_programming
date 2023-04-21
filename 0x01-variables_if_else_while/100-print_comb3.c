e48080de36a074371569b624957f0465aed8d134
GoodnessJames <akomagoodness97@gmail.com>
2023-04-21 12:55:20 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,13 +11,13 @@ int main(void)
 
 	int num2;
 
-	for (num1 = 0; num1 < 10; num1++)
+	for (num1 = 0; num1 < 7; num1++)
 	{
-		for (num2 = 0; num2 < 10; num2++)
+		for (num2 = 1; num2 < 8; num2++)
 		{
 			putchar((num1 % 10) + '0');
 			putchar((num2 % 10) + '0');
-		if (num1 == 9 && num2 == 9)
+		if (num1 == 6 && num2 == 7)
 			continue;
 		putchar(',');
 		putchar(' ');
