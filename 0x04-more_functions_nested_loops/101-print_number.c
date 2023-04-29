5d9880be1d456105357b748e3e1a04953a2912f9
GoodnessJames <akomagoodness97@gmail.com>
2023-04-28 19:41:07 -0700

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * void print_number - Prints an integer using the _putchar function
+ * @n: Integer to print
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int i = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -i;
+	}
+
+	if ((i / 10) > 0)
+		print_number(i / 10);
+	_putchar((i % 10) + '0');
+}
