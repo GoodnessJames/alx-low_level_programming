83966a00c7d8c32620b80aa06eb04dfa24fced25
GoodnessJames <akomagoodness97@gmail.com>
2023-04-28 17:29:32 -0700

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square followed by a new line on the terminal
+ * @size: The number of times the character \ should be printed
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+	int i;
+
+	if  (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			int j;
+
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
