96324a4f052953a35fbfd384b5f01b13d347b53e
GoodnessJames <akomagoodness97@gmail.com>
2023-05-04 15:01:29 -0700

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,18 +11,17 @@
 int main(void)
 {
 	const char characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=";
-	char password[PASSWORD_ELEMENTS + 1];
+	/*char password[PASSWORD_ELEMENTS + 1];*/
 	int index;
 
 	srand(time(NULL));
 
 	for (index = 0; index < PASSWORD_ELEMENTS; index++)
-		password[index] = characters[rand() % (sizeof(characters) - 1)];
+		printf("%c", charset[rand() % (sizeof(charset) - 1)]);
+		/*password[index] = characters[rand() % (sizeof(characters) - 1)];*/
 
-	password[PASSWORD_ELEMENTS] = '\0';
+	/*password[PASSWORD_ELEMENTS] = '\0';*/
+	printf("\n");
 
-	if (sizeof(password) != PASSWORD_ELEMENTS)
-		printf("Tada! Congrats\n");
-	
 	return (0);
 }
