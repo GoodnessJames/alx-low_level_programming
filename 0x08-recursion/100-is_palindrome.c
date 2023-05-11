9bf7c0550927487a2f28de178b050f3c256ff891
GoodnessJames <akomagoodness97@gmail.com>
2023-05-11 15:45:42 -0700

    Recursion in C

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,17 +1,36 @@
 #include "main.h"
+#include <string.h>
+
+int check_palindrome(char *s, int start_index, int end_index);
+int is_palindrome(char *s);
 
 /**
- * is_palindrome - Returns 1 if a string is a palindrome and 0 if not
- * @s: Pointer to a string
+ * check_palindrome - Checks if a string is a palindrome
+ * @s: Pointer to a string to check
+ * @start_index: The first index of the string
+ * @end_index: The last index of the string
  *
- * Return: Returns ((n == 2) ? 1 : 0), (1), or (0).
+ * Return: (1), (0) or (check_palindrome(s, start_index + 1, end_index - 1))
+ */
+int check_palindrome(char *s, int start_index, int end_index)
+{
+	if (start_index >= end_index)
+		return (1);
+	if (s[start_index] != s[end_index])
+		return (0);
+	return (check_palindrome(s, start_index + 1, end_index - 1));
+}
+
+/**
+ * is_palindrome - Checks if a string is a palindrome
+ * @s: Pointer to a string to check
+ *
+ * Return: Returns (check_palindrome(s, start_index, end_index))
  */
 int is_palindrome(char *s)
 {
-	int string_start, string_end;
+	int start_index =  0;
+	int end_index = strlen(s) - 1;
 
-	if (string_start >= string_end)
-		return (1);
-	if (s[string_start] == s[string_end])
-		return (is_palindrome(s, string_start + 1, string_end - 1));
+	return (check_palindrome(s, start_index, end_index));
 }
