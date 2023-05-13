c66fcfc47be846b68312531af09f1dae6e982df0
GoodnessJames <akomagoodness97@gmail.com>
2023-05-13 16:59:56 -0700

    C - argc, argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -20,12 +20,9 @@ int main(int argc, char *argv[])
 	}
 	cents = atoi(argv[1]);
 
-	if (cents <= 0)
-	{
-		printf("0\n");
-		return (0);
-	}
 	coins = 0;
+
+	coins += cents / 25;
 	cents %= 25;
 	coins += cents / 10;
 	cents %= 10;
