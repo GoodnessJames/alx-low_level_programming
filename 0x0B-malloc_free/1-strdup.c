c7cefa3c7f96f7c1eb03fe66de63853f05f3ed65
GoodnessJames <akomagoodness97@gmail.com>
2023-05-16 14:05:41 -0700

    Malloc and free function in C

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,18 +12,17 @@ char *_strdup(char *str)
 	int len, i;
 	char *ptStr;
 
+	if (ptStr == NULL)
+		return (NULL);
 	len = 0;
 	while (str[len] != '\0')
 		len++;
 	ptStr = (char *)malloc((len + 1) * sizeof(char));
 
-	if (ptStr == NULL)
-		return (NULL);
-
 	for (i = 0; str[i] != '\0'; i++)
 		ptStr[i] = str[i];
 
-	ptStr[i] = '\0';
+	ptStr[len] = '\0';
 
 	return (ptStr);
 }
