a15841d6b246ca8ea294ff06508b53e29d9985ff
GoodnessJames <akomagoodness97@gmail.com>
2023-05-24 12:46:57 -0700

    Function pointers in C

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,25 @@
+#include "calc.h"
+
+/**
+ * op_add - Returns the sum of two numbers
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: returns (a + b)
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Returns the difference of two numbers 
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: returns (a - b)
+ */
+int op_sub(int a, int b)
+{
+
+}
