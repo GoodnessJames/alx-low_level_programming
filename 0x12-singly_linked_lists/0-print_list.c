507a9a80fc398ab0fc5aa6db1d17f9cc1d903e24
GoodnessJames <akomagoodness97@gmail.com>
2023-06-02 01:32:15 -0700

    Fix the bug for task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,16 +4,17 @@
  * print_list - This function prints all the elements of a list_t list
  * @h: Pointer to the linked list (list_t)
  *
- * Return: On SUCCESS, returns (nodes) which is the number of nodes
- *		   On FAILURE, returns (-1) which signifies an error
+ * Return: ALWAYS return (nodes) which is the number of nodes
  */
 size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
 	const list_t *tmp;
 
+	if (h == NULL)
+		return (nodes);
 	tmp = h;
-	while (tmp)
+	while (tmp != NULL)
 	{
 		if (tmp->str == NULL)
 			printf("[0] (nil)\n");
