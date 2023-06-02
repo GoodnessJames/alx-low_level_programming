a1a2ed89a326306b21601eeab9f1eb387bd613ae
GoodnessJames <akomagoodness97@gmail.com>
2023-06-02 02:58:28 -0700

    Singly linked lists in C

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * beforeMain - Prints a string before the main function is executed
+ *
+ * Return: Returns nothing (void)
+ */
+void beforeMain(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
