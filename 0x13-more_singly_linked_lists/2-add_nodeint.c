d8f88f934c4bf966dbf42568c24b1ee2af0039b7
GoodnessJames <akomagoodness97@gmail.com>
2023-06-05 16:41:22 -0700

    More problems on singly linked list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -20,9 +20,8 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	if (head == NULL)
 		return (NULL);
 	newNode->next = *head;
-	
+
 	*head = newNode;
 
 	return (newNode);
 }
-
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - Adds a new node at the end of a listint_t list
+ * add_nodeint_end - Adds a new node at the end of the listint_t list
+ * @head: Pointer to the first node of the listint_t list
  * @n: Integer
  *
  * Return: On SUCCESS, return (endNode) i.e. the address of the new element
