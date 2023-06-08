266c034b6fb78ad3eb1625050786de6a04cd73cf
GoodnessJames <akomagoodness97@gmail.com>
2023-06-07 17:43:35 -0700

    Fix the bug in task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,30 +1,87 @@
 #include "lists.h"
 
+size_t count_listint_nodes(const listint_t *head);
+size_t print_listint_safe(const listint_t *head);
+
 /**
- * print_listint_safe - This function prints a listint_t list
- * @head: Pointer to the first node of the linked list (listint_t)
+ * count_listint_nodes - Counts the number of nodes in listint_t list
+ * @head: Pointer to the first node of the listint_t
  *
- * Return: On SUCCESS, returns (nodes) i.e. the number of nodes in the list
- *		   On FAILURE, returns (0) with an exit status of 98.
+ * Return: On SUCCESS, returns (nodes) i.e the number of nodes in the list
+ *         On FAILURE, returns (0)
+ */
+size_t count_listint_nodes(const listint_t *head)
+{
+	const listint_t *tmp1, *tmp2;
+	size_t nodes = 1;
+
+	if (head == 0 || head->next == 0)
+		return (0);
+
+	tmp1 = head->next;
+	tmp2 = (head->next)->next;
+
+	while (tmp2)
+	{
+		if (tmp1 == tmp2)
+		{
+			tmp1 = head;
+			while (tmp1 != tmp2)
+			{
+				nodes++;
+				tmp1 = tmp1->next;
+				tmp2 = tmp2->next;
+			}
+
+			tmp1 = tmp1->next;
+			while (tmp1 != tmp2)
+			{
+				nodes++;
+				tmp1 = tmp1->next;
+			}
+
+			return (nodes);
+		}
+
+		tmp1 = tmp1->next;
+		tmp2 = (tmp2->next)->next;
+	}
+
+	return (0);
+}
+
+/**
+ * print_listint_safe - Prints a listint_t list safely.
+ * @head: A pointer to the first node of the listint_t list.
+ *
+ * Return: Returns (nodes) i.e the number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nodes = 0;
-	const listint_t *tmp;
+	size_t nodes, i = 0;
 
-	if (head == NULL)
-		return (0);
-	tmp = head;
-	while (tmp)
+	nodes = count_listint_nodes(head);
+
+	if (nodes == 0)
 	{
-		printf("[%p] %d\n", (void *)tmp, tmp->n);
-		nodes++;
-		if (tmp->next >= tmp)
+		while (head != 0)
 		{
-			printf("-> [%p] %d\n", (void *)tmp->next, tmp->next->n);
-			exit(98);
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+			nodes++;
 		}
-		tmp = tmp->next;
 	}
+
+	else
+	{
+		for (i = 0; i < nodes; i++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+
+		printf("-> [%p] %d\n", (void *)head, head->n);
+	}
+
 	return (nodes);
 }
