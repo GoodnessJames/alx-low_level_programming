356b1a12e39377b033317652b4d0e073991cfb08
GoodnessJames <akomagoodness97@gmail.com>
2023-06-13 15:14:42 -0700

    Fix the bug in task 1 causing check7 code output fail

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,13 +21,19 @@ int create_file(const char *filename, char *text_content)
 		while (text_content[len])
 			len++;
 	}
-
 	o = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (!text_content)
+	{
+		close(o);
+		return (1);
+	}
 	w = write(o, text_content, len);
 
-	if (o < 0 || w < 0)
+	if (o == -1 || w == -1 || w != len)
+	{
+		close(o);
 		return (-1);
-
+	}
 	close(o);
 
 	return (1);
