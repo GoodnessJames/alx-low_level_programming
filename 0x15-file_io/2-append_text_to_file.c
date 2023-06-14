dd6865f5d5092a823e148869fdc7ee098cc78c07
GoodnessJames <akomagoodness97@gmail.com>
2023-06-14 08:41:07 -0700

    Fix the bug in task 2 causing check6 code output fail

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -26,7 +26,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	o = open(filename, O_WRONLY | O_APPEND);
 	w = write(o, text_content, len);
 
-	if (o == -1 || w == -1)
+	if (o == -1 || w == -1 || w != len)
 		return (-1);
 
 	close(o);
