692892141917c5c55b6322beec75eea31fbdd473
Goodness James <akomagoodness97@gmail.com>
2023-06-16 20:08:08 +0100

    Debugging in C

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* File: main.h
+ * Desc: A header file that contains all the function prototypes for the project - 0x03-debugging
+ */
+
+/* Standard C Header file */
 #include <stdio.h>
 
+/* Function prototypes */
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
+
 #endif /* MAIN_H */
