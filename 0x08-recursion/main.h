86061f24a8a187e919a53552bca5473ba6d12501
GoodnessJames <akomagoodness97@gmail.com>
2023-07-01 06:32:35 -0700

    Update header file

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,11 @@
-
+#ifndef MAIN_H
 #define MAIN_H
+
+/*
+ * File: main.h
+ * Desc: A Header file containing all the function prototypes
+ *		 used in the project - 0x08-recursion
+ */
 
 int _putchar(char c);
 void _puts_recursion(char *s);
