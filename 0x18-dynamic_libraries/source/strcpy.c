fdb15e5ca127481a10760532ec124d5ff493e344
GoodnessJames <akomagoodness97@gmail.com>
2023-07-24 14:07:49 +0100

    Move all source files to source folder

---
+++ 0x18-dynamic_libraries/source/strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strcpy - Copies the string pointed to by src, including the terminating
+ * null byte (\0), to the buffer pointed to by dest
+ * @dest: The pointer to the destination buffer
+ * @src: Pointer to the string to be copied
+ *
+ * Return: Returns the value @dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0;
+	int index = 0;
+
+	while (src[len] != '\0')
+		len++;
+	for (; index < len; index++)
+		dest[index] = src[index];
+	dest[len] = '\0';
+	return (dest);
+}
