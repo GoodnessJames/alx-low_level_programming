52854b1910ca0dcd33d18bcd182a7a86029123c7
GoodnessJames <akomagoodness97@gmail.com>
2023-07-24 10:47:36 +0100

    Dynamic libraries in C

---
+++ 0x18-dynamic_libraries/abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _abs - Computes the absolute value of an integer
+* @n: The character to compute
+*
+* Return: Return n or absoluteValue
+*/
+int _abs(int n)
+{
+	if (n < 0)
+		n = n * -1;
+
+	return (n);
+}
+
---
+++ 0x18-dynamic_libraries/memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * *_memcpy - Copies memory area
+ * @dest: Pointer to the destination buffer
+ * @src: Pointer to the source to copy from
+ * @n: Number of bytes to copy
+ *
+ * Return: Return (dest)
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int count;
+
+	for (count = 0; count < n; count++)
+		dest[count] = src[count];
+	return (dest);
+}
